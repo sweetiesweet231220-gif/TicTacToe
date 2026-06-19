40d392562b92cc470e5a29b5d56eb1c83db2ef8d
ORSU ANJALI <sweetiesweet231220@gmail.com>
2026-06-19 16:14:38 +0530

    Clear box innerText when enabling boxes

--- app.js
+++ app.js
@@ -57,7 +57,7 @@ const disableBoxes = () => {
 const enableBoxes = () => {
     for(let box of boxes){
         box.disabled = false;
-        box.innerText = empty;
+        box.innerText = "";
     }
 }; 
 
